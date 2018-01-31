b66a6c763767f75854d5cfee722e763677f6b4ef
smythili75 <31503935+smythili75@users.noreply.github.com>
2018-01-31 11:15:23 +0000

    Create minm.c

---
+++ minm.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,a[n],t;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	i=0;
+		if(a[i]<a[i+1]&&a[i]<a[i+2])
+		{
+			t=a[i];
+		}
+		else if(a[i+1]<a[i+2]&&a[i+1]<a[i])
+		{
+			t=a[i+1];
+		}
+		else
+		{
+			t=a[i+2];
+		}
+			printf("%d",t);
+
+}
