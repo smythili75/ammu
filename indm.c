ea86f6c5907e96ba8249d3cb01f6546ce0d43eab
smythili75 <31503935+smythili75@users.noreply.github.com>
2018-02-03 21:30:37 +0530

    Create indm.c

---
+++ indm.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,a[i];
+	scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+	printf(" \n %d %d \n",a[i],i);
+	
+}
+}
+	
