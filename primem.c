657af79032e4e6e75b93265859d722e4155b0c12
smythili75 <31503935+smythili75@users.noreply.github.com>
2018-02-26 10:24:50 +0000

    Create primem.c

---
+++ primem.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,b=0;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			b=b+1;
+		}
+	}
+	if(b==2)
+	{
+		printf("prime");
+	}
+	else
+	{
+		printf("not prime");
+	}
+}
