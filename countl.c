261b3e35c4cc2ad77fee4c15d6d878334a45ec9e
smythili75 <31503935+smythili75@users.noreply.github.com>
2018-02-26 10:58:53 +0000

    Create countl.c

---
+++ countl.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int count=1,i=0;
+	char s1[12]="hello world.123";
+	while(s1[i]!='\0')
+	{
+		if(s1[i]=='.')
+		{
+			count=count+1;
+		}
+		i++;
+	}
+	printf("%d",count);
+}
